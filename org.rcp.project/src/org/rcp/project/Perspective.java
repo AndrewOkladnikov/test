f7f90779a2fa68a6c4cee5c0c2b9ea283196ff5f
AndrewOkladnikov <ao19641510@mail.ru>
2022-02-12 15:38:16 +0700

    first commit

---
+++ org.rcp.project/src/org/rcp/project/Perspective.java
@@ -0,0 +1,11 @@
+package org.rcp.project;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override	
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
